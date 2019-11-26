0de60ee94cd7aac2b95b2c4559f63fd1c9c5b39f
Heronnes Pereira <heronnestg@hotmail.com>
2019-11-26 17:07:16 -0300

    react 13

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import classes from './App.css'
+
+import Pessoa from './Pessoa/Pessoa'; 
+
+class App extends Component {
+
+  state = {  
+    pessoas: [
+     {id:'1', nome: 'Tom', idade: 21},
+     {id:'2', nome: 'Julia', idade: 22},
+     {id:'3', nome: 'William', idade: 23}
+    ],
+    outroState: 'algum outro valor',
+    mostrarPessoas: false
+  }  
+  
+  nomeManipuladorAlterado = (event, id) => {
+    const pessoaIndex = this.state.pessoas.findIndex(p => {
+      return p.id === id;
+    });
+    
+    const pessoa = {
+      ...this.state.pessoas[pessoaIndex]
+    };
+
+    // alternativa para a l de cima const pessoa = Object.assign({}, this.state.pessoas[pessoaIndex]);
+
+    pessoa.nome = event.target.value;
+
+    const pessoas = [...this.state.pessoas];
+    pessoas[pessoaIndex] = pessoa;
+
+    this.setState({pessoas: pessoas});
+  }
+
+  apagarManipuladorPessoa = (pessoaIndex) => {
+    //const pessoas = this.state.pessoas.slice();
+    const pessoas = [...this.state.pessoas];
+    pessoas.splice(pessoaIndex, 1);
+    this.setState({pessoas: pessoas});
+  }
+
+  toogleManipuladorPessoas = () => {
+    const fazerMostrar = this.state.mostrarPessoas;
+    this.setState({mostrarPessoas: !fazerMostrar});
+  }
+
+  render() {
+    
+    let pessoas = null;
+    let botaoClasse = '';
+
+    if (this.state.mostrarPessoas) {
+      pessoas = (
+        <div>
+        {this.state.pessoas.map((pessoa, index) => {
+          return <Pessoa 
+          click={() => this.apagarManipuladorPessoa(index)}
+          nome={pessoa.nome} 
+          idade={pessoa.idade}
+          key={pessoa.id}
+          changed={(event) => this.nomeManipuladorAlterado(event, pessoa.id)}/>
+        })}
+        </div>
+      );
+      
+      botaoClasse = classes.vermelho;
+    }
+
+    const classesAssinaladas = [];
+    if (this.state.pessoas.length <= 2) {
+      classesAssinaladas.push(classes.vermelho);  //classes = ['vermelho']
+    }
+    if (this.state.pessoas.length <= 1) {
+      classesAssinaladas.push(classes.negrito);  //classes = ['vermelho', 'negrito']
+    }
+
+    return (
+      
+      <div className={classes.App}>
+        <h1>Olá, Sou um aplicativo React</h1>
+        <p className={classesAssinaladas.join( ' ' )}>Isto está funcionando</p>
+        <button 
+        className={botaoClasse}
+        onClick={this.toogleManipuladorPessoas}>Alternancia de Pessoas
+        </button> 
+        {pessoas}
+      </div> 
+    ); 
+  }
+}
+
+export default App;
+
